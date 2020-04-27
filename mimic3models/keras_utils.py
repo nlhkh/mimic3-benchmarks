6459717383a002e94801ee6d7ed94bc41204962a
Kha Nguyen <kha.nguyen@futurice.com>
2020-04-27 10:41:51 +0300

    print model dropout, change regularizers for dropout and weight loss

--- mimic3models/keras_utils.py
+++ mimic3models/keras_utils.py
@@ -523,3 +523,17 @@ class MCBCUtils:
     @classmethod
     def score_avg(cls, prop):
         return np.mean(prop, axis=1)
+
+
+def print_dropout_rates(model):
+    for layer in model.layers:
+        if hasattr(layer, 'dropout'):
+            if layer.dropout == 1.0:
+                print(layer.name, 'dropout =', layer.p.numpy())
+            else:
+                print(layer.name, 'dropout =', layer.p)
+        if hasattr(layer, 'recurrent_dropout'):
+            if layer.recurrent_dropout == 1.0:
+                print(layer.name, 'recurrent_dropout =', layer.p_r.numpy())
+            else:
+                print(layer.name, 'recurrent_dropout =', layer.p_r)
